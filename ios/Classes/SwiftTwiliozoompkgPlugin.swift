4eb15e5e124ae16728f461967f78cb5656076c55
apple <mbhanu@enoahisolution.com>
2021-02-10 18:56:05 +0530

    Adding the ios changes

--- ios/Classes/SwiftTwiliozoompkgPlugin.swift
+++ ios/Classes/SwiftTwiliozoompkgPlugin.swift
@@ -2,13 +2,21 @@ import Flutter
 import UIKit
 
 public class SwiftTwiliozoompkgPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "twiliozoompkg", binaryMessenger: registrar.messenger())
-    let instance = SwiftTwiliozoompkgPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
+public static func register(with registrar: FlutterPluginRegistrar) {
+  let channel = FlutterMethodChannel(name: "twiliozoompkg", binaryMessenger: registrar.messenger())
+  let instance = SwiftTwiliozoompkgPlugin()
+  registrar.addMethodCallDelegate(instance, channel: channel)
+}
 
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+  print("Native Method Fired")
+  switch call.method {
+    case "connectTwilio":
+      result("Twilio Connected.")
+    case "connectZoom":
+      result("connectZoom Connected.")
+    default:
+      result(FlutterMethodNotImplemented)
+    } 
   }
 }
